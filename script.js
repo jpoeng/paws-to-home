2c042bb1ee226e251fe596da6b37d994e80533b8
Jennifer Poeng <jpoeng@brightedge.com>
2018-11-04 15:08:26 -0800

    Add Lightbox for Bootstrap library and apply it to image gallery

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
-// Load local .json file and build image gallery
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var data = JSON.parse(this.responseText);
-        var dogList = data.dogs;
-
-        for (var i = dogList.length - 1; i >= 0; i--) {
-            var imageContainer = document.createElement('div');
-            imageContainer.classList.add('dog', 'col-xs-12', 'col-sm-4');
-            document.getElementById("gallery").appendChild(imageContainer);
-
-            var image = document.createElement('img');
-            image.setAttribute('src', dogList[i].image);
-            image.classList.add('img-fluid');
-            imageContainer.appendChild(image);
-        }
+/**
+ * Helper functions
+ * 1. setAttribute()
+ */
+function setAttributes(el, attrs) {
+    for(var key in attrs) {
+        el.setAttribute(key, attrs[key]);
     }
-};
-xmlhttp.open('GET', 'assets/data/dogs.json', true);
-xmlhttp.send();
+}
+
+/**
+ * Lightbox for Bootstrap
+ * Description: Opens images in modal on click
+ * Source: http://ashleydw.github.io/lightbox/
+ */
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+/**
+ * Image Gallery of Adorable Dogs <3
+ * 1. Load local json file
+ * 2. Build image gallery
+ */
+function buildGallery() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            var dogList = data.dogs;
+
+            for (var i = dogList.length - 1; i >= 0; i--) {
+                var imgUrl = dogList[i].image;
+                var imgUrlSource = dogList[i].source;
+
+                var imageContainer = document.createElement('a');
+                imageContainer.classList.add('dog', 'col-xs-12', 'col-sm-4');
+                setAttributes(imageContainer, {
+                    'data-toggle': 'lightbox',
+                    'data-type': 'image',
+                    'data-footer': 'source: <a href="' + imgUrlSource + '">' + imgUrlSource + '</a>',
+                    'href': imgUrl
+                });
+                document.getElementById("gallery").appendChild(imageContainer);
+
+                var image = document.createElement('img');
+                image.setAttribute('src', imgUrl);
+                image.classList.add('img-fluid');
+                imageContainer.appendChild(image);
+            }
+        }
+    };
+    xmlhttp.open('GET', 'assets/data/dogs.json', true);
+    xmlhttp.send();
+}
+buildGallery();
